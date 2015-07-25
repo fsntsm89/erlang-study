815c47006c215eb5fe6e13cfcaf225025652e77a
Keisuke Ito <ito@komatsuelec.co.jp>
2015-07-25 17:55:33 +0900

    chapter10

---
+++ chapter10/usr_db.erl
@@ -0,0 +1,72 @@
+%%%-------------------------------------------------------------------
+%%% @author Keisuke Ito <ito@pc036.komatsuelec.co.jp>
+%%% @copyright (C) 2015, Keisuke Ito
+%%% @doc
+%%%
+%%% @end
+%%% Created : 23 Jul 2015 by Keisuke Ito <ito@pc036.komatsuelec.co.jp>
+%%%-------------------------------------------------------------------
+-module(usr_db).
+-include("usr.hrl").
+
+%% API
+-export([create_tables/1, close_tables/0, add_usr/1, update_usr/1,
+         lookup_id/1, lookup_msisdn/1, restore_backup/0]).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+create_tables(FileName) ->
+    ets:new(usrRam, [named_table, {keypos, #usr.msisdn}]),
+    ets:new(usrIndex, [named_table]),
+    dets:open_file(usrDisk, [{file, FileName}, {keypos, #usr.msisdn}]).
+
+close_tables() ->
+    ets:delete(usrRam),
+    ets:delete(usrIndex),
+    dets:close(usrDisk).
+
+add_usr(#usr{msisdn=PhoneNo, id=CustId} = Usr) ->
+    ets:insert(usrIndex, {CustId, PhoneNo}),
+    update_usr(Usr).
+
+update_usr(Usr) ->
+    ets:insert(usrRam, Usr),
+    dets:insert(usrDisk, Usr),
+    ok.
+
+lookup_id(CustId) ->
+    case get_index(CustId) of
+        {ok, PhoneNo} -> lookup_msisdn(PhoneNo);
+        {error, instance} -> {error, instance}
+    end.
+
+lookup_msisdn(PhoneNo) ->
+    case ets:lookup(usrRam, PhoneNo) of
+        [Usr] -> {ok, Usr};
+        [] -> {error, instance}
+    end.
+
+restore_backup() ->
+    Insert = fun(#usr{msisdn=PhoneNo, id=Id} = Usr) ->
+                     ets:insert(usrRam, Usr),
+                     ets:insert(usrIndex, {Id, PhoneNo}),
+                     continue
+             end,
+    dets:traverse(usrDisk, Insert).
+
+delete_disabled() ->
+    ets:safe_fixtable(usrRam, true),
+    catch loop_delete_disabled(ets:first(usrRam)),
+    ets:safe_fixtable(usrRam, false),
+    ok.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+get_index(CustId) ->
+    case ets:lookup(usrIndex, CustId) of
+        [{CustId, PhoneNo}] -> {ok, PhoneNo};
+        [] -> {error, instance}
+    end.
